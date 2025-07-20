0f7f72aa3b55e2f025509d383991dc06053eb103
Ahnaf Arafat <ahnafarafat.py@gmail.com>
2025-07-20 01:10:21 +0000

    Step 5.3: Added DRF views for Plans, Subscriptions, Invoices, Payments

--- django_app/billing/views.py
+++ django_app/billing/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Plan, Subscription, Invoice, Payment
+from .serializers import PlanSerializer, SubscriptionSerializer, InvoiceSerializer, PaymentSerializer
 
-# Create your views here.
+# Plans: Everyone can see
+class PlanListView(generics.ListAPIView):
+    queryset = Plan.objects.all()
+    serializer_class = PlanSerializer
+
+
+# Subscriptions: List and Create
+class SubscriptionListCreateView(generics.ListCreateAPIView):
+    queryset = Subscription.objects.all()
+    serializer_class = SubscriptionSerializer
+
+
+# Invoices: List only
+class InvoiceListView(generics.ListAPIView):
+    queryset = Invoice.objects.all()
+    serializer_class = InvoiceSerializer
+
+
+# Payments: List only
+class PaymentListView(generics.ListAPIView):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+
